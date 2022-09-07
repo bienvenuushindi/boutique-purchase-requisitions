7d46a2e984c8b710034aa53f707c7685b60ab406
usbbush <jeanbienvenusb@gmail.com>
2022-09-07 13:22:03 +0200

    Remove some eslint error

--- src/data/dummy.js
+++ src/data/dummy.js
@@ -1,67 +1,67 @@
 const products = [
-    {
-        id: 1,
-        name: 'product name',
-        price: {
-            unit: 1,
-            dozen: 2,
-            box: 3
-        },
-        quantity: {
-            box_units: 12,
-            per_unit: 10,
-        },
-        urls: {
-            images:['image_url']
-        }
-    }
+  {
+    id: 1,
+    name: 'product name',
+    price: {
+      unit: 1,
+      dozen: 2,
+      box: 3,
+    },
+    quantity: {
+      box_units: 12,
+      per_unit: 10,
+    },
+    urls: {
+      images: ['image_url'],
+    },
+  },
 ];
 
 const providers = [
-    {
-        id: 1,
-        name: 'shop name',
-        address: {
-            city: 'city',
-            avenue: 'avenue',
-            building: {
-                name: 'building name',
-                floor: 'floor',
-                number: 1
-            }
-        },
-        urls: {
-            images:['image_url']
-        },
-        category_ids:[1,2]
-    }
+  {
+    id: 1,
+    name: 'shop name',
+    address: {
+      city: 'city',
+      avenue: 'avenue',
+      building: {
+        name: 'building name',
+        floor: 'floor',
+        number: 1,
+      },
+    },
+    urls: {
+      images: ['image_url'],
+    },
+    category_ids: [1, 2],
+  },
 ];
 
-const productCategories=[
-    {
-        id: 1,
-        name:'category 1',
-    }
+const productCategories = [
+  {
+    id: 1,
+    name: 'category 1',
+  },
 ];
 
 const providersProducts = [
-    {
-        provider_id: 1,
-        products_ids: [
-            1, 2, 3, 4
-        ],
-    }
+  {
+    provider_id: 1,
+    products_ids: [
+      1, 2, 3, 4,
+    ],
+  },
 ];
 
 const requisitons = {
-    'date-time': [{
-        product_id: 1,
-        status: 'pending', // can be also completed and uncompleted
-        expect_on:'date', // In case the product is not found but there is a known arrival date
-        provider_id:1,
-        quantity: {
-            [type]: 2
-        }, // type can be 'box, dozen, units'
-        price: 2
-    }],
+  'date-time': [{
+    product_id: 1,
+    status: 'pending', // can be also completed and uncompleted
+    expect_on: 'date', // In case the product is not found but there is a known arrival date
+    provider_id: 1,
+    quantity: {
+      [type]: 2,
+    }, // type can be 'box, dozen, units'
+    price: 2,
+  }],
 };
--- src/index.js
+++ src/index.js
@@ -1,20 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import {BrowserRouter} from "react-router-dom";
-import {Provider} from "react-redux";
+import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
 import './index.css';
 import App from './App';
 // import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-    <React.StrictMode>
-        <Provider store={store}>
-            <BrowserRouter>
-                <App/>
-            </BrowserRouter>
-        </Provider>
-    </React.StrictMode>
+  <React.StrictMode>
+    <Provider store={store}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </Provider>
+  </React.StrictMode>,
 );
 
 // // If you want to start measuring performance in your app, pass a function
